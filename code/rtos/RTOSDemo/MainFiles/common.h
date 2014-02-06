b20528027e88e846e8aec33773fbc514e1e2fa2b
Kevin Malachowski <kevin@chowski.com>
2014-02-06 11:40:45 -0500

    Added the ability to make timers quickly.

--- code/rtos/RTOSDemo/MainFiles/common.h
+++ code/rtos/RTOSDemo/MainFiles/common.h
@@ -29,7 +29,7 @@
 #define FATAL(x) VT_HANDLE_FATAL_ERROR(x)
 #endif
 
-#define FATALSTR(str) { \
+#define FATALSTR(str) VT_HANDLE_FATAL_ERROR(0); { \
 		void LCDwriteLn(int line, char* data); \
 		LCDwriteLn(8, str); \
 		/* Hacky way to help make sure the LCD will draw correctly */ \
@@ -38,7 +38,7 @@
 	}
 
 // This macro helps with checking return codes
-#define FAILIF(x) if (x) FATAL(0);
+#define FAILIF(x) if ((x)) FATAL(0);
 
 #define SLEEP(ms) vTaskDelay(ms/portTICK_RATE_MS)
 
@@ -56,43 +56,58 @@
 #define SEND(chan, src)  if(xQueueSend(chan, &src, portMAX_DELAY) != pdTRUE) FATAL(0);
 
 
-// Use this to define a task (usually in a header file)
-#define TASK_PROTOTYPE(name, type, stackSize, priority) \
+// Helper defines, used internally in this common.h. They are considered private and should not
+//    be used outside of this file.
+#define __stip(name, inside, period) \
+	unsigned short const name##PERIOD = period; \
+	xTimerHandle Make##name(inside)
+#define __stap(name, inside, stackSize, priority) \
 	unsigned short const name##STACK_SIZE = stackSize; \
 	unsigned portBASE_TYPE const name##PRIORITY = priority; \
-	void Start##name(type* ps);
+	void Start##name(inside)
 
-#define TASK_PROTOTYPE_NOARG(name, stackSize, priority) \
-	unsigned short const name##STACK_SIZE = stackSize; \
-	unsigned portBASE_TYPE const name##PRIORITY = priority; \
-	void Start##name(void);
+// Use this to declare a timer callback prototype (usually in a header file)
+#define TIMER_PROTOTYPE(name, type, period) __stip(name, type *ps, period)
+#define TIMER_PROTOTYPE_NOARG(name, period) __stip(name,     void, period)
 
-// Use this to declare a task (usually in a source (.c) file)
+// Use this to declare a task (usually in a header file)
+#define TASK_PROTOTYPE(name, type, stackSize, priority) __stap(name, type* ps, stackSize, priority);
+#define TASK_PROTOTYPE_NOARG(name, stackSize, priority) __stap(name,     void, stackSize, priority);
+
+#define __stif(name, inside, createParam) \
+	static void name(xTimerHandle pxTimer); \
+	xTimerHandle Make##name(inside) { \
+		xTimerHandle retval; \
+		FAILIF((retval = xTimerCreate((const signed char*) #name, name##PERIOD, pdTRUE, createParam, name)) == NULL); \
+		return retval; \
+	} \
+	static void name(xTimerHandle pxTimer) {
+#define __staf(name, inside, createParam, funcParamName) \
+	static portTASK_FUNCTION(name, orig_##funcParamName); \
+	void Start##name(inside) { \
+		portBASE_TYPE retval; \
+		if ((retval = xTaskCreate(name, (signed char*) #name, name##STACK_SIZE, createParam, name##PRIORITY, NULL )) != pdPASS) { \
+			FATAL(retval); \
+		} \
+	} \
+	static portTASK_FUNCTION(name, orig_##funcParamName) {
+
+// Use this to define a timer (usually in a source (.c) file)
+#define TIMER_FUNC(name, type, paramName) __stif(name, type* ps, ps) \
+											DOESN'T WORK yet; '\
+											type* paramName = (type*)orig_##paramName;
+#define TIMER_FUNC_NOARG(name)            __stif(name, void, NULL);
+#define ENDTIMER }
+// Use this to define a task (usually in a source (.c) file)
 // NOTE: You need an asscociated TASK_PROTOTYPE for every TASK_FUNC, (yes, even if it's in the same file).
 //   But Usually the TASK_FUNC goes in a .c file and the TASK_PROTOTYPE goes in a .h file.
-#define TASK_FUNC(name, type, paramName) \
-	static portTASK_FUNCTION(name, orig_##paramName); \
-	void Start##name(type* ps) { \
-		portBASE_TYPE retval; \
-		if ((retval = xTaskCreate(name, (signed char*) #name, name##STACK_SIZE, ps, name##PRIORITY, NULL )) != pdPASS) { \
-			FATAL(retval); \
-		} \
-	} \
-	static portTASK_FUNCTION(name, orig_##paramName) { \
-		type* paramName = (type*)orig_##paramName;
-
-
-#define TASK_FUNC_NOARG(name) \
-	static portTASK_FUNCTION(name, unusedParam); \
-	void Start##name(void) { \
-		portBASE_TYPE retval; \
-		if ((retval = xTaskCreate(name, (signed char*) #name, name##STACK_SIZE, NULL, name##PRIORITY, NULL )) != pdPASS) { \
-			FATAL(retval); \
-		} \
-	} \
-	static portTASK_FUNCTION(name, unusedParam) {
-		
+#define TASK_FUNC(name, type, paramName) __staf(name, type* ps, ps, paramName)	\
+                                         	type* paramName = (type*)orig_##paramName;
+#define TASK_FUNC_NOARG(name)            __staf(name, void, NULL, unusedParam)
 #define ENDTASK }
+
+
+#define START_TIMER(handle, blockticks) FAILIF(xTimerStart(handle,blockticks) != pdPASS)
 
 #define INSPECT_STACK(max_stack) \
 	unsigned portBASE_TYPE InitialStackLeft = uxTaskGetStackHighWaterMark(NULL); \
--- code/rtos/RTOSDemo/MainFiles/sensors.c
+++ code/rtos/RTOSDemo/MainFiles/sensors.c
@@ -17,66 +17,24 @@ static vtConductorStruct conduct;
 #include "tasks.h"
 #include "klcd.h"
 
-
-
-
-TASK_PROTOTYPE_NOARG(PumpSensor, 1000, tskIDLE_PRIORITY);
-
 #define mainI2CMONITOR_TASK_PRIORITY			( tskIDLE_PRIORITY)
 void StartSensorTasks() {
 	// Temp sensor buffer (for milestone 1)
 	FAILIF(vtI2CInit(&vtI2C0,0,mainI2CMONITOR_TASK_PRIORITY,100000) != vtI2CInitSuccess);
 
 	vStarti2cTempTask(&vtTemp, tskIDLE_PRIORITY, &vtI2C0);
-	StartPumpSensor();
+	START_TIMER(MakePumpSensor(), 0);
 	vStartConductorTask(&conduct, tskIDLE_PRIORITY, &vtI2C0, &vtTemp);
 }
-				  
-#define tempWRITE_RATE_BASE	( ( portTickType ) 500 / portTICK_RATE_MS)
-TASK_FUNC_NOARG(PumpSensor) {
-	int count = 0;	
-	while(1) {
-		vTaskDelay(500/portTICK_RATE_MS);
-		SendTempTimerMsg(&vtTemp,tempWRITE_RATE_BASE,0);
-		if (count++ % 40 == 0) {
-			char buf[2] = {(count/10)%10 + '0', 0};
-			LCDwriteLn(3, buf);
-		}
-	}
-} ENDTASK
-/*
-void TempTimerCallback(xTimerHandle pxTimer)
-{
-	if (pxTimer == NULL) {
-		VT_HANDLE_FATAL_ERROR(0);
-	} else {
-		// When setting up this timer, I put the pointer to the 
-		//   Temperature structure as the "timer ID" so that I could access
-		//   that structure here -- which I need to do to get the 
-		//   address of the message queue to send to 
-		vtTempStruct *ptr = (vtTempStruct *) pvTimerGetTimerID(pxTimer);
-		// Make this non-blocking *but* be aware that if the queue is full, this routine
-		// will not care, so if you care, you need to check something
-		if (SendTempTimerMsg(ptr,tempWRITE_RATE_BASE,0) == errQUEUE_FULL) {
-			// Here is where you would do something if you wanted to handle the queue being full
-			VT_HANDLE_FATAL_ERROR(0);
-		}
-	}
-}
 
-void startTimerForTemperature(vtTempStruct *vtTempdata) {
-	if (sizeof(long) != sizeof(vtTempStruct *)) {
-		VT_HANDLE_FATAL_ERROR(0);
+TIMER_FUNC_NOARG(PumpSensor) {
+	static int count = 0;
+	SendTempTimerMsg(&vtTemp,PumpSensorPERIOD,0);
+	if ((count++ % 40) == 0) {
+		char buf[2] = {(count/10)%10 + '0', 0};
+		LCDwriteLn(3, buf);
 	}
-	xTimerHandle TempTimerHandle = xTimerCreate((const signed char *)"Temp Timer",tempWRITE_RATE_BASE,pdTRUE,(void *) vtTempdata,TempTimerCallback);
-	if (TempTimerHandle == NULL) {
-		VT_HANDLE_FATAL_ERROR(0);
-	} else {
-		if (xTimerStart(TempTimerHandle,0) != pdPASS) {
-			VT_HANDLE_FATAL_ERROR(0);
-		}
-	}
-}*/
+} ENDTIMER
 
 
 
