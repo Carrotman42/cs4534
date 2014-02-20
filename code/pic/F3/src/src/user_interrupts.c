5f00084c4622bbb06b6acf10d71f96fb2eab59cf
Michael <mcantrell522@gmail.com>
2014-02-20 05:22:22 -0500

    UART Transmitting 0x55 every 10ms

--- code/pic/F3/src/src/user_interrupts.c
+++ code/pic/F3/src/src/user_interrupts.c
@@ -16,7 +16,6 @@
 
 void timer0_int_handler() {
 
-
 #ifdef SENSOR_PIC
     ADCON0bits.GO = 1;
     WriteTimer0(0xFFFF-375);
@@ -27,16 +26,16 @@ void timer0_int_handler() {
 // This one does the action I wanted for this program on a timer1 interrupt
 
 void timer1_int_handler() {
-    unsigned int result;
-
+//    unsigned int result;
     // read the timer and then send an empty message to main()
 #ifdef __USE18F2680
     LATBbits.LATB1 = !LATBbits.LATB1;
 #endif
-
-    result = ReadTimer1();
+    debugNum(4);
+    uart_send((char) 0x55);
+//    result = ReadTimer1();
     //ToMainLow_sendmsg(0, MSGT_TIMER1, (void *) 0);
 
     // reset the timer
-    WriteTimer1(0);
+    WriteTimer1(0xFFFF-3750);
 }
