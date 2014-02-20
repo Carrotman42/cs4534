4a5c41b580c84ff830f523f729b6086602ab17b4
Kevin Malachowski <kevin@chowski.com>
2014-02-19 21:00:06 -0500

    Attempted to make a bytequeue. Untested.

---
+++ code/pic/F3/src/src/byte_q.c
@@ -0,0 +1,50 @@
+
+#include "byte_q.h"
+
+void vqInit(q*byte_queue) {
+	q->write = q->read = 0;
+}
+
+// NOTE: if write==read it is empty. This is achieved because it
+//    is not possible to fully fill the queue's buffer (we'd have
+//    to waste a byte somewhere if we ever want a buffer > 128 anyway)
+//    doing it this way makes it easier to program
+int vqSend(q*byte_queue, unsigned len, unsigned char *data) {
+	// Make sure it can fit
+	unsigned char w = q->write, r = q->read;
+	unsigned char cap;
+	// Deal with unsigned math carefully
+	if (r > w) {
+		cap = r - w;
+	} else /*if (r <= w)*/ {
+		cap = VAR_Q_LEN - (w - r);
+	}
+	if (cap <= len) return 1;
+	
+	unsigned char* dest = &q->data[0];
+	unsigned char last = (w + len) & VAR_Q_MASK;
+	while (w != last) {
+		dest[w] = *data++;
+		w = (w + 1) & VAR_Q_MASK;
+	}
+	
+	q->write = w;
+	return 0;
+}
+
+inline unsigned char vqPop(q*byte_queue, char *ok) {
+	// Set whether there is any data to read
+	register unsigned char r = q->read;
+	if (*ok = (q->write != r)) {
+		unsigned char ret = q->data[r];
+		q->read = (r + 1) & VAR_Q_MASK;
+		return ret;
+	}
+	return 0;
+}
+
+
+
+
+
+
---
+++ code/pic/F3/src/src/byte_q.h
@@ -0,0 +1,21 @@
+#ifndef BYTE_Q_H_INC
+#define BYTE_Q_H_INC
+
+#define varqBits 7
+#define VAR_Q_LEN (1 << varqBits)
+#define VAR_Q_MASK (VAR_Q_LEN - 1)
+typedef struct {
+	unsigned char write;
+	unsigned char read;
+	unsigned char data[VAR_Q_LEN];
+} byte_queue;
+
+void vqInit(q*byte_queue);
+int vqSend(q*byte_queue, unsigned len, unsigned char *data);
+
+// ok is an out param. If it is true then there was a value, else the queue
+//    was empty.
+inline unsigned char vqPop(q*byte_queue, char *ok);
+
+
+#endif
