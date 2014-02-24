33455c639eb3a97051d4f0a0fba148c97cb3b496
Glen Polo <glen.polo@gmail.com>
2014-02-24 02:36:29 -0500

    Transmit/Receive done, '1,2,3,4, \r' is the correct message while '1,3,2,4,\r' is the incorrect message.  They are alternating messages every 10 ms. 1

--- code/pic/F3/src/PIC_Framework_3.X/build/UseXC8/production/_ext/1360937237/my_uart.p1.d
+++
@@ -1,7 +0,0 @@
- build/UseXC8/production/_ext/1360937237/my_uart.d  \
- build/UseXC8/production/_ext/1360937237/my_uart.p1:  \
- ../src/my_uart.c  \
-../src/debug.h  \
-../src/my_uart.h  \
-../src/maindefs.h  \
-../src/messages.h 
--- code/pic/F3/src/PIC_Framework_3.X/build/UseXC8/production/_ext/746215922/brain_rover.p1.d
+++
@@ -1,7 +0,0 @@
- build/UseXC8/production/_ext/746215922/brain_rover.d  \
- build/UseXC8/production/_ext/746215922/brain_rover.p1:  \
- ../../../../common/communication/brain_rover.c  \
-../../../../common/communication/brain_rover.h  \
-../../../../common/communication/../sensor_types.h  \
-../../../../common/communication/../common.h  \
-../../../../common/communication/../../pic/F3/src/src/debug.h 
