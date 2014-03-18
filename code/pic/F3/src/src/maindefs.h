7c95af52d683d043a2be9903f796d8c33fad1b45
David Kindel <dkindel@vt.edu>
2014-03-18 11:17:22 -0400

    high priority cases handled

--- code/pic/F3/src/src/maindefs.h
+++ code/pic/F3/src/src/maindefs.h
@@ -65,11 +65,11 @@
 
 #define DEBUG_ON
 
-//#define ARM_EMU //arm emulator (really simple, just sends commands on a timer)
+#define ARM_EMU //arm emulator (really simple, just sends commands on a timer)
 //#define ROVER_EMU //rover emulator (really simple, just sends dummy values back over uart and sends back frame data on a timer)
 
 //#define PICMAN
-#define SENSOR_PIC
+//#define SENSOR_PIC
 //#define MOTOR_PIC
 //#define MASTER_PIC
 
